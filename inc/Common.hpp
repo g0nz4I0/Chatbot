1544b6ea9a1e7d10f21a8f059894d8c2331412e7
Gonzalo Sanchez <gonzalosmoya24@gmail.com>
2024-04-16 18:46:43 +0200

    unify common headers

---
+++ inc/Common.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+// C++ includes
+
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <stdexcept>
+#include <exception>
+#include <source_location>
+#include <format>
+#include <errno.h>
+#include <algorithm>
+#include <atomic>
+namespace fs = std::filesystem;
+
+// Socket includes
+#ifdef _WIN32
+#else
+    #include <sys/types.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <ifaddrs.h>
+    #include <unistd.h>
+#endif
